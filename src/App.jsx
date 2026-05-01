5c7ce544bcb5b950380367f35248da742c0583b1
RedrovanD <166569832+RedrovanD@users.noreply.github.com>
2026-05-01 03:02:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,8 @@
-
+import { Link, Route, Routes } from 'react-router-dom';
+import { Fish, PlusCircle } from 'lucide-react';
+import { useState, useEffect } from "react";
+import Home from './pages/Home.jsx';
+import CreatePost from './pages/CreatePost.jsx';
+import PostDetail from './pages/PostDetail.jsx';
+import EditPost from './pages/EditPost.jsx';
+import FishFinder from "./pages/FishFinder.jsx";
